5261960c668751d5a02e62be5ac53c7e11e797e0
Amar <69308291+amarfiguig@users.noreply.github.com>
2023-03-02 23:37:27 +0000

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,12 @@
-// Require the express router
 const router = require('express').Router();
+const apiRoutes = require('./api');
 
-// Import the user-routes module
-const userRoutes = require('./user-routes');
+// Set up API routes
+router.use('/api', apiRoutes);
 
-// Import the thought-routes module
-const thoughtRoutes = require('./thought-routes');
+// Catch all other routes and send an error message
+router.use((req, res) => {
+  return res.send('Wrong route!');
+});
 
-// Add a middleware that handles all routes for '/users' and delegates them to userRoutes module
-router.use('/users', userRoutes);
-
-// Add a middleware that handles all routes for '/thoughts' and delegates them to thoughtRoutes module
-router.use('/thoughts', thoughtRoutes);
-
-// Export the router to make it available to other modules
 module.exports = router;
