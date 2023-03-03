acd6224d1218b6a0aaec7263bd5e69309c5b9829
Amar <69308291+amarfiguig@users.noreply.github.com>
2023-03-03 21:04:13 +0000

    Update Thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -1,39 +1,80 @@
-const { Schema, model } = require("mongoose");
-const reactionSchema = require("./Reaction");
+// Import Mongoose Schema and Model
+const { Schema, model } = require('mongoose');
 
-// Schema to create Thoughts model
-const thoughtSchema = new Schema(
-  {
-    thoughtText: {
+// Reaction class constructor
+class Reaction {
+  constructor({ reactionBody, username }) {
+    this.reactionBody = reactionBody;
+    this.username = username;
+    this.createdAt = Date.now();
+  }
+}
+
+// Thought class constructor
+class Thought {
+  constructor({ thoughtText, username, reactions = [] }) {
+    this.thoughtText = thoughtText;
+    this.username = username;
+    this.createdAt = Date.now();
+    // Map through reactions array and create a new Reaction object for each reaction
+    this.reactions = reactions.map(reaction => new Reaction(reaction));
+  }
+
+  // Getter to get the length of the reactions array
+  get reactionCount() {
+    return this.reactions.length;
+  }
+}
+
+// Define thoughtSchema using Mongoose Schema
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: 'You need to leave a thought',
+    minlength: 1,
+    maxlength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    // Format createdAt timestamp into a string
+    get: timestamp => timestamp.toLocaleString(),
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [new Schema({
+    reactionBody: {
       type: String,
-      required: [true, "You must leave a thought"],
-      minlength: 1,
+      required: true,
       maxlength: 280,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: (createdAt) => createdAt.toLocaleString(),
     },
     username: {
       type: String,
       required: true,
     },
-    reaction: [reactionSchema],
-  },
-  //This allows the JSON representation of the object to include the getters, but not the id.
-  {
-    toJSON: {
-      getters: true,
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // Format createdAt timestamp into a string
+      get: timestamp => timestamp.toLocaleString(),
     },
-    id: false,
-  }
-);
-
-thoughtSchema.virtual("reactionCount").get(function () {
-  return this.reaction.length;
+  }, { _id: false })]
+}, { 
+  // Include getters and virtuals when converting to JSON
+  toJSON: { getters: true, virtuals: true },
+  // Exclude the _id field from the schema
+  id: false 
 });
 
-const Thought = model("Thought", thoughtSchema);
+// Virtual getter to get the length of the reactions array
+thoughtSchema.virtual('reactionCount').get(function(){
+  return this.reactions.length;
+});
 
+// Create a new Mongoose model named "Thought" based on the thoughtSchema
+const ThoughtModel = model('Thought', thoughtSchema);
+
+// Export the Thought class
 module.exports = Thought;
