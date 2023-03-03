17868312be31d4f9f6a130a38c467305c2064b11
Amar <69308291+amarfiguig@users.noreply.github.com>
2023-03-03 20:59:38 +0000

    Update Reaction.js

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,34 +1,42 @@
-const { Schema, Types } = require("mongoose");
+// Import Mongoose library
+const mongoose = require('mongoose');
 
-// Schema only - refered in the thought model
-const reactionSchema = new Schema(
-  {
-    reactionId: {
-      type: Schema.Types.ObjectId,
-      default: () => new Types.ObjectId(),
-    },
-    reactionBody: {
-      type: String,
-      required: true,
-      maxlength: 280,
-    },
-    username: {
-      type: String,
-      required: true,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now(),
-      get: (createdAt) => createdAt.toLocaleString(),
-    },
+// Define Reaction schema using Mongoose.Schema
+const reactionSchema = new mongoose.Schema({
+  // Define reactionId field with a default value of a new ObjectID
+  reactionId: {
+    type: mongoose.Types.ObjectId,
+    default: () => mongoose.Types.ObjectId(),
   },
-  //This allows the JSON representation of the object to include the getters, but not the id.
-  {
-    toJSON: {
-      getters: true,
-    },
-    id: false,
-  }
-);
+  // Define reactionBody field as a required string with a maximum length of 280 characters
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  // Define username field as a required string
+  username: {
+    type: String,
+    required: true,
+  },
+  // Define createdAt field as a Date with a default value of the current time and a custom getter function to format the date as a string
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAt) => createdAt.toLocaleString(),
+  },
+});
 
-module.exports = reactionSchema;
+// Set options for the toJSON method of the schema, including removing the _id and id fields from the returned object
+reactionSchema.set('toJSON', {
+  getters: true,
+  virtuals: false,
+  versionKey: false,
+  transform: function (doc, ret) {
+    delete ret._id;
+    delete ret.id;
+  },
+});
+
+// Export the Reaction model using the reactionSchema
+module.exports = mongoose.model('Reaction', reactionSchema);
